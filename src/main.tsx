f8aa59d6730a20f57e2365714c952e01585e6e8b
Juan Alberto <jarscode.dev@outlook.com>
2024-11-27 01:38:22 +0100

    fix: component routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { AuthProvider } from '@/context/AuthContext'
+import { AuthProvider } from './context/AuthContext'
 import { MotionConfig } from 'framer-motion'
 
 createRoot(document.getElementById('root')!).render(
