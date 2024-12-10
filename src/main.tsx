6d666eec343cada7d47dd4b561274c734a97f094
Juan Alberto <jarscode.dev@outlook.com>
2024-12-10 20:27:12 +0100

    fix: syntax errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,16 @@
 import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { MotionConfig } from 'framer-motion'
-import { KindeProvider } from "@kinde-oss/kinde-auth-react";
+import App from './App'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <KindeProvider
-    clientId="556f3e9ae9ed4b5c90c5dde853235518"
-    domain="https://notabussiness.kinde.com"
-    redirectUri="http://localhost:5173"
-    logoutUri="http://localhost:5173"
-  >
-    <Router>
-      <AuthProvider>
-        <MotionConfig transition={{ duration: 0.4, ease: "easeInOut" }}>
-          <App />
-        </MotionConfig>
-      </AuthProvider>
-    </Router>
-  </KindeProvider>
+  <Router>
+    <AuthProvider>
+      <MotionConfig transition={{ duration: 0.4, ease: "easeInOut" }}>
+        <App />
+      </MotionConfig>
+    </AuthProvider>
+  </Router>
 )
