22cc8ea7b98b2736b00349abc2c5a33e64a38c19
Juan Alberto <jarscode.dev@outlook.com>
2024-12-10 23:48:28 +0100

    fix: hero button scale only on lg and md screen sizes

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -20,7 +20,7 @@ export const Hero = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6, ease: "linear" }}
       >
-        <p className="w-full mx-auto text-lg text-center text-black sm:max-w-screen-2xl dark:text-white sm:text-center">
+        <p className="w-full mx-auto text-lg font-medium leading-6 text-neutral-950 sm:max-w-screen-xl dark:text-white sm:text-center">
           Bee is an open-source tool designed to simplify the creation, organization, and tracking of URLs.
           With an intuitive interface and powerful features, Bee allows you to manage links efficiently,
           saving time and enhancing the analysis of your URLs' performance. Streamline your URL management
@@ -32,7 +32,7 @@ export const Hero = () => {
             href="https://github.com/302FoundDev/bee-frontend"
             rel="noopener"
             target="_blank"
-            className="flex items-center justify-center w-full gap-2 py-2 mx-auto text-base transition bg-blue-700 rounded-full hover:scale-105 sm:max-w-48 hover:bg-blue-800"
+            className="flex items-center justify-center w-full gap-2 py-2 mx-auto text-base transition bg-blue-700 rounded-lg sm:text-lg sm:hover:scale-105 sm:max-w-48 hover:bg-blue-800"
           >
             <FaSquareGithub />
             Star on GitHub
